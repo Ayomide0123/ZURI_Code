4a11067e1823cf6bba06640563939f9986163ab5
Ayomide0123 <64165980+Ayomide0123@users.noreply.github.com>
2022-05-30 21:25:08 +0100

    Update anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -3,14 +3,17 @@
 # find_anagrams("hello", "check") --> False
 # find_anagrams("below", "elbow") --> True
 
+
 def find_anagram(word, anagram):
     # [assignment] Add your code here
-    
-    if(sorted(word) == sorted(anagram)):
-        print("The strings are anagrams.")
-    else:
-        print("The strings aren't anagrams.")        
 
+    if sorted(word) == sorted(anagram):
+        return True
+    else: 
+        return False
+    
+    
 word = input ("Enter first word: ")
 anagram = input ("Enter anagram: ")
-find_anagram(word, anagram)
+print(find_anagram(word, anagram))
+
